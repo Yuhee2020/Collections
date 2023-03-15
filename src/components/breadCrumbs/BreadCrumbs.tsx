421969c1448ba53d0ff29b5078bcf89a8ccac74e
Yukhimuk Dzmitry <yuhimuk.d@gmail.com>
2023-03-15 10:14:18 +0300

    refactoring breadCrumbs

--- src/components/breadCrumbs/BreadCrumbs.tsx
+++ src/components/breadCrumbs/BreadCrumbs.tsx
@@ -4,42 +4,52 @@ import { Breadcrumb } from 'antd'
 import { useTranslation } from 'react-i18next'
 import { NavLink } from 'react-router-dom'
 
-import { useAppSelector } from '../../hooks'
+import { useAppDispatch, useAppSelector } from '../../hooks'
 import { ADMIN_PAGE, BIGGEST_COLLECTIONS, ROOT, USER_PAGE } from '../../pages/rotes/Rotes'
+import { setSearch } from '../../store/reducers/itemsReducer'
 
 import s from './BreadCrumbs.module.scss'
 
 export const BreadCrumbs = () => {
+  const dispatch = useAppDispatch()
   const isLoggedIn = useAppSelector(state => state.auth.isLogin)
   const isAdmin = useAppSelector(state => state.auth.isAdmin)
+  const searchText = useAppSelector(state => state.items.searchText)
   const { t } = useTranslation()
 
   const changeClassname = ({ isActive }: { isActive: boolean }) =>
     isActive ? s.activeNavLink : s.navLink
+  const handleCrumbClick = () => {
+    searchText && dispatch(setSearch(''))
+  }
 
   return (
     <Breadcrumb>
       <Breadcrumb.Item>
-        <NavLink className={changeClassname} to={ROOT}>
+        <NavLink onClick={handleCrumbClick} className={changeClassname} to={ROOT}>
           {' '}
           {t('lastAdded')}
         </NavLink>
       </Breadcrumb.Item>
       <Breadcrumb.Item>
-        <NavLink className={changeClassname} to={BIGGEST_COLLECTIONS}>
+        <NavLink
+          onClick={handleCrumbClick}
+          className={changeClassname}
+          to={BIGGEST_COLLECTIONS}
+        >
           {t('largestCollection')}
         </NavLink>
       </Breadcrumb.Item>
       {isLoggedIn && (
         <Breadcrumb.Item>
-          <NavLink className={changeClassname} to={USER_PAGE}>
+          <NavLink onClick={handleCrumbClick} className={changeClassname} to={USER_PAGE}>
             {t('userPage')}
           </NavLink>
         </Breadcrumb.Item>
       )}
       {isAdmin && isLoggedIn && (
         <Breadcrumb.Item>
-          <NavLink className={changeClassname} to={ADMIN_PAGE}>
+          <NavLink onClick={handleCrumbClick} className={changeClassname} to={ADMIN_PAGE}>
             {t('adminPage')}
           </NavLink>
         </Breadcrumb.Item>
